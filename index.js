be225a191e9075ba47fac71ccaf58de517a3fc65
Yolanda Morris <y.mijoe7145@gmail.com>
2021-12-16 23:11:06 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = "Wonder Woman";// Write your code in this file!
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+
